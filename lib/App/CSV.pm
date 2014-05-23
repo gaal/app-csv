831f593b4ec9282b529324d896c98bdf5ab3b643
Gaal Yahas <gaal@forum2.org>
2014-05-23 10:38:19 +0300

    * App-CSV, version 0.8

--- lib/App/CSV.pm
+++ lib/App/CSV.pm
@@ -4,7 +4,7 @@ use Moose;
 use IO::Handle;
 use Text::CSV;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 BEGIN {
   # One day, MooseX::Getopt will allow us to pass pass_through to Getopt::Long.
